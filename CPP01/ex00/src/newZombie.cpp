b9108c905f2823cc3dc737799b5eb457dd80caf4
Ondřej Běhávka <obehavka@student.42heilbronn.de>
2024-11-29 17:15:45 +0100

    small changes in exercise CPP01_ex00 to be in line with subject

--- CPP01/ex00/src/newZombie.cpp
+++ CPP01/ex00/src/newZombie.cpp
@@ -6,13 +6,13 @@
 /*   By: obehavka <obehavka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:12:39 by obehavka          #+#    #+#             */
-/*   Updated: 2024/11/29 16:13:02 by obehavka         ###   ########.fr       */
+/*   Updated: 2024/11/29 16:39:08 by obehavka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* newZombie(const std::string& name)
+Zombie* newZombie(const std::string name)
 {
 	return (new Zombie(name));
 }
--- CPP01/ex00/src/randomChump.cpp
+++ CPP01/ex00/src/randomChump.cpp
@@ -6,13 +6,13 @@
 /*   By: obehavka <obehavka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:13:39 by obehavka          #+#    #+#             */
-/*   Updated: 2024/11/29 16:13:44 by obehavka         ###   ########.fr       */
+/*   Updated: 2024/11/29 16:39:05 by obehavka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	randomChump(const std::string& name)
+void	randomChump(const std::string name)
 {
 	Zombie zombie(name);
 	zombie.announce();
