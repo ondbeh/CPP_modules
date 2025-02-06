2132dd3ba85023f9f7db3b0909fc6e69f125852e
behavond <behavond@fit.cvut.cz>
2025-02-06 11:46:52 +0100

    Files for exerecise ex01

---
+++ CPP02/ex01/src/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obehavka <obehavka@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 10:56:43 by obehavka          #+#    #+#             */
+/*   Updated: 2025/02/06 10:56:43 by obehavka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) : _value(value << _fractionalBits)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : _value(roundf(value * (1 << _fractionalBits)))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Assignation operator called" << std::endl;
+    if (this == &other)
+        return (*this);
+    _value = other.getRawBits();
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (_value);
+}
+
+void Fixed::setRawBits(int const new_value)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    _value = new_value;
+}
+
+int Fixed::toInt(void) const
+{
+    return (_value >> _fractionalBits);
+}
+
+float Fixed::toFloat(void) const
+{
+    return (static_cast<float>(_value) / static_cast<float>(1 << _fractionalBits));
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+    out << fixed.toFloat();
+    return (out);
+}
---
+++ CPP02/ex01/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obehavka <obehavka@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 11:19:37 by obehavka          #+#    #+#             */
+/*   Updated: 2025/02/06 11:19:37 by obehavka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Fixed.hpp"
+
+int main(void)
+{
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return (0);
+}
